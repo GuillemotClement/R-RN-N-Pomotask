ae61af9ba8a2a48946a939b2e8c09116888e7712
GuillemotClement <guillemot.clement@protonmail.com>
2025-05-05 05:36:54 +0200

    notification

--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -1,4 +1,8 @@
+import { faPauseCircle, faPlayCircle } from "@fortawesome/free-regular-svg-icons";
+import { faRotateRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function Timer() {
   const [time, setTime] = useState(0);
@@ -7,6 +11,8 @@ export default function Timer() {
   const [duration, setDuration] = useState(0); // saisis user de temps souhaiter
   const [second, setSecond] = useState(0); // contient la duree
   const [isRunning, setIsRunning] = useState(false); // lance le compteur
+
+  const notify = () => toast("Periode terminer");
 
   const handlePerdiode = (formData: any) => {
     const duringTimer = formData.get("duration");
@@ -24,6 +30,7 @@ export default function Timer() {
           if (prev + 1 >= duration) {
             clearInterval(interval);
             setIsRunning(false); // Stoppe le timer
+            notify();
             return duration; // Fixe la durée maximale
           }
           return prev + 1;
@@ -54,18 +61,19 @@ export default function Timer() {
 
   return (
     <div className='border w-96'>
+      <ToastContainer />
       <h2 className='text-center font-bold uppercase'>Timer</h2>
       <p>Duree du timer: {duration}</p>
       <p>Temps ecoule :{second}</p>
       <div className='flex justify-center gap-x-2'>
         <button className='btn btn-primary' onClick={handleStartTimer}>
-          Start
+          <FontAwesomeIcon icon={faPlayCircle} />
         </button>
         <button className='btn btn-warning' onClick={handleStopTimer}>
-          Pause
+          <FontAwesomeIcon icon={faPauseCircle} />
         </button>
         <button className='btn btn-error' onClick={handleResetTimer}>
-          Reset
+          <FontAwesomeIcon icon={faRotateRight} />
         </button>
       </div>
 
